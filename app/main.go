dc87b698c48ccca1540aa409f1c10a162c4a13c4
Kristina Iermolenko <kristina.iermolenko@points.com>
2025-11-05 08:09:49 -0700

    indefinite prompting

--- app/main.go
+++ app/main.go
@@ -7,12 +7,14 @@ import (
 )
 
 func main() {
-	fmt.Fprint(os.Stdout, "$ ")
-	// capture the user's command in the "command" variable
-	command, err := bufio.NewReader(os.Stdin).ReadString('\n')
-	if err != nil {
-		fmt.Println("error capturing the command.")
-		return
+	for {
+		fmt.Fprint(os.Stdout, "$ ")
+		// capture the user's command in the "command" variable
+		command, err := bufio.NewReader(os.Stdin).ReadString('\n')
+		if err != nil {
+			fmt.Println("error capturing the command.")
+			return
+		}
+		fmt.Println(command[:len(command)-1] + ": command not found")
 	}
-	fmt.Println(command[:len(command)-1] + ": command not found")
 }

7f6f24ae4b77d60a270cd7dccc34f89940e1a62f
Kristina Iermolenko <kristina.iermolenko@points.com>
2025-11-05 09:30:35 -0700

    exit command

--- app/main.go
+++ app/main.go
@@ -4,6 +4,8 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strconv"
+	"strings"
 )
 
 func main() {
@@ -15,6 +17,21 @@ func main() {
 			fmt.Println("error capturing the command.")
 			return
 		}
-		fmt.Println(command[:len(command)-1] + ": command not found")
+		command = command[:len(command)-1]
+		mainCommand := ""
+		var args []string
+		if len(command) != 0 {
+			args = strings.Split(command, " ")
+			mainCommand = args[0]
+			args = args[1:]
+		}
+		if mainCommand == "exit" {
+			v, err := strconv.Atoi(args[0])
+			if err != nil {
+				fmt.Println("incorrect command arguments: ", command)
+			}
+			os.Exit(v)
+		}
+		fmt.Println(command + ": command not found")
 	}
 }
